8b74eb2102b4b907e3ab7f073eeb284b220ba9f7
styhrd <juanmiguelcaparas76@gmail.com>
2024-11-17 16:53:03 +0800

    Trying to fetch data

---
+++ codingTest/src/GameData.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import ABLImage from './assets/GameLogo/ABL.webp';
+import PopImage from './assets/GameLogo/PoP.webp';
+import SRImage from './assets/GameLogo/SR.webp';
+import BBWQImage from './assets/GameLogo/BBW.webp';
+import BLImage from './assets/GameLogo/BL.webp';
+import SCImage from './assets/GameLogo/SC.webp';
+import BOEImage from './assets/GameLogo/BOE.webp';
+import PPImage from './assets/GameLogo/PP.webp';
+import CBImage from './assets/GameLogo/CB.webp';
+import AWImage from './assets/GameLogo/AW.webp';
+import MJImage from './assets/GameLogo/MJ.webp';
+import IJImage from './assets/GameLogo/IJ.webp';
+
+export const GameData = [
+    { name: 'Pride of Persia', img: PopImage, category: 'jackpot', isNew: false, isFavorite: false, gameProvider: 'playtech' },
+    { name: 'Sugar Rush', img: SRImage, category: 'jackpot', isNew: false, isFavorite: false, gameProvider: 'PragmaticPlay' },
+    { name: 'Big Bro World', img: BBWQImage, category: 'live', isNew: false, isFavorite: false, gameProvider: 'playtech' },
+    { name: 'Beach Life', img: BLImage, category: 'slots', isNew: false, isFavorite: false, gameProvider: 'playtech' },
+    { name: 'Shaolin Crew', img: SCImage, category: 'slots', isNew: false, isFavorite: false, gameProvider: 'Xpanz' },
+    { name: 'Book of Egypt', img: BOEImage, category: 'table', isNew: false, isFavorite: false, gameProvider: 'Xpanz' },
+    { name: 'Pirates Power', img: PPImage, category: 'others', isNew: false, isFavorite: false, gameProvider: 'Xpanz' },
+    { name: 'Crocodile Blitz', img: CBImage, category: 'jackpot', isNew: false, isFavorite: false, gameProvider: 'playtech' },
+    { name: 'Anaconda Wild 2', img: AWImage, category: 'slots', isNew: false, isFavorite: false, gameProvider: 'playtech' },
+    { name: 'Maya Jackpot', img: MJImage, category: 'jackpot', isNew: false, isFavorite: false, gameProvider: 'skywind' },
+    { name: 'Azteca Bonus Lines', img: ABLImage, category: 'bingo', isNew: false, isFavorite: false, gameProvider: 'playtech' },
+    { name: 'Inca Jackpot', img: IJImage, category: 'jackpot', isNew: false, isFavorite: false, gameProvider: 'skywind' }
+]
